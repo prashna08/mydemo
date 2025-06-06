f89ec6bf77f7d46cc5fbad3a8c2ee4be6c57a951
prashna08 <prashnarai8@gmail.com>
2025-06-06 15:43:26 +0545

    Add files via upload

--- Jersey/jersey.js
+++ Jersey/jersey.js
@@ -1,3 +1,4 @@
+const navLinks = document.querySelectorAll(".nav-list .nav-link")
 const menuOpenButton = document.querySelector("#menu-open-button")
 const menuCloseButton = document.querySelector("#menu-close-button")
 
@@ -9,3 +10,28 @@ menuOpenButton.addEventListener("click", () => {
 //toogle to hide the menu
 menuCloseButton.addEventListener("click", () => 
 menuOpenButton.click())
+
+//toogle to hide the nav-bar when clicked on a link
+navLinks.forEach(link =>{
+    link.addEventListener("click", () => menuOpenButton.click());
+})
+
+//animation
+window.addEventListener('scroll', animated);
+
+function animated() {
+    var animated = document.querySelectorAll('.animated');
+    for (var i = 0; i < animated.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = animated[i].getBoundingClientRect().top;
+        var elementVisible = 130;
+
+        if (elementTop < windowHeight - elementVisible) {
+            animated[i].classList.add('active');
+        } else {
+            animated[i].classList.remove('active');
+        }
+    }
+}
+
+
